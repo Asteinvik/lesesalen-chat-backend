ae1fe35e4ca7df60336a1309ccfcfee7e151ba37
andrmos <mossige.a@gmail.com>
2018-05-04 20:10:25 +0200

    Add method to receive message

--- src/flasktest.py
+++ src/flasktest.py
@@ -1,13 +1,17 @@
 from flask import Flask, render_template
-from flask_socketio import SocketIO, emit
+from flask_socketio import SocketIO, send, emit
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
 
+if __name__ == '__main__':
+    socketio.run(app)
+
 @app.route('/')
 def index():
     return ('hello world!')
 
-if __name__ == '__main__':
-    socketio.run(app)
+@socketio.on('message')
+def handle_message(message):
+    print('received message: ' + message)
