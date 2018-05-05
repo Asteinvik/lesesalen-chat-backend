5593ae516841e063e6a03e5d625ee86366a07c30
andrmos <mossige.a@gmail.com>
2018-05-05 13:03:25 +0200

    Add database url

--- src/flasktest.py
+++ src/flasktest.py
@@ -1,9 +1,16 @@
+import os
 from flask import Flask, render_template
 from flask_socketio import SocketIO, send, emit
+from flask_sqlalchemy import SQLAlchemy
+
+DATABASE_URL = os.environ['DATABASE_URL']
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
 socketio = SocketIO(app)
+db = SQLAlchemy(app)
+
 
 if __name__ == '__main__':
     socketio.run(app)
