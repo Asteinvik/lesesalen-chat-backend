bca7cde74de6569f24968f0900a2eadcb5b4ee51
andrmos <mossige.a@gmail.com>
2018-05-05 13:51:11 +0200

    Use pipenv

--- src/flasktest.py
+++ src/flasktest.py
@@ -7,7 +7,8 @@ DATABASE_URL = os.environ['DATABASE_URL']
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URL
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 socketio = SocketIO(app)
 db = SQLAlchemy(app)
 
