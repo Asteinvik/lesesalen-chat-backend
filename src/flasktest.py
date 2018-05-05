7bc0ff339eda2d0c2eb7ddb2e691f4ec587034c5
steinvik <steinvik@protours.no>
2018-05-05 18:15:38 +0200

    add rooms

--- src/flasktest.py
+++ src/flasktest.py
@@ -1,12 +1,15 @@
 import os
 from flask import Flask, render_template, request, json
-from flask_socketio import SocketIO, send, emit
+from flask_socketio import SocketIO, send, emit, join_room, leave_room
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from datetime import datetime
-
+import random as rnd
+import math
 
 DATABASE_URL = os.environ['DATABASE_URL']
+
+rooms=[]
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
@@ -92,6 +95,29 @@ def handle_message(message):
 def connect():
     id = 1
     emit('on_connect', {'data': 'You are connected. ID: %d' % id})
+
+@socketio.on('join')
+def on_join(data):
+	username = data['username']
+	user=db.session.query(User).filter_by(username=username).first()
+    room = rooms[user.id]
+    join_room(room)
+    send(username + ' has entered the room.', room=room)
+
+
+
+def giveRoom(){
+	nUsers=db.session.query(User).order_by(User.id).count()
+	nRooms=math.ceil(nUsers/2)
+	roomspace=(list(range(1,nRooms+1))+list(range(1,nRooms+1)))
+	rooms=[0]*1000
+	for user in db.session.query(User).order_by(User.id):
+		rooms[user.id]=rnd.choice(roomspace)
+		print(user.id,rooms[user.id])
+
+
+
+}
 
 
 
